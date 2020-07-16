991fad44663a6e01a81843f8bf70662a9cbf817a
Ubuntu <ubuntu@ip-172-31-19-145.us-east-2.compute.internal>
2020-07-16 23:00:45 +0000

    Minor

--- pycontrast/training_bashes/train_res18_yf.sh
+++ pycontrast/training_bashes/train_res18_yf.sh
@@ -2,4 +2,4 @@ python main_contrast.py \
   --method InfoMin \
   --cosine \
   --data_folder /data5/chengxuz/Dataset/yfcc/jpgs_in_imagenet_format \
-  --multiprocessing-distributed --world-size 1 --rank 0 --arch resnet18 --dist-url tcp://127.0.0.1:23400
+  --multiprocessing-distributed --world-size 1 --rank 0 --arch resnet18 --dist-url tcp://127.0.0.1:23400 "$@"
